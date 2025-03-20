4f74ad9b2f73b7e40c7cdda82aab7f8287537842
ashish kumar singh <ashish159singh@gmail.com>
2025-03-20 17:11:51 +0530

    crud-delete op added

--- React/practice1/components/CRUD/CrudOp.jsx
+++ React/practice1/components/CRUD/CrudOp.jsx
@@ -4,15 +4,21 @@ import ListData from "./ListData";
 
 function CrudOp() {
   const [dataList, setDataList] = useState([]);
-
+  console.log(dataList)
+  
   function addData(data) {
-    setDataList([...dataList, data]);
+    setDataList([...dataList, { item: data, id: dataList.length + 1 }]);
+  }
+
+  function handleDelete(id){
+    const newDataList = dataList.filter((data)=>data.id !== id);
+    setDataList(newDataList);
   }
 
   return (
     <>
       <AddData addData={addData}></AddData>
-      <ListData dataList={dataList}></ListData>
+      <ListData dataList={dataList} handleDelete={handleDelete}></ListData>
     </>
   );
 }
--- React/practice1/components/CRUD/ListData.jsx
+++ React/practice1/components/CRUD/ListData.jsx
@@ -1,11 +1,17 @@
-function ListData({ dataList }) {
+function ListData({ dataList, handleDelete }) {
+  // function handleDelete() {
+  //   console.log(dataList(data.id));
+  // }
+  // console.log(id);
   return (
     <ul>
       {dataList.map((data) => {
         return (
-          <div>
-            <li>{data}</li>
-            <button type="button">X</button>
+          <div key={data.id}>
+            <li>{data.item}</li>
+            <button type="button" onClick={()=>handleDelete(data.id)}>
+              X
+            </button> 
             <button type="button">clear</button>
           </div>
         );
